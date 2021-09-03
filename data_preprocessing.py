c0264e5ff447bb9d3aeb3b18637f2184154626b1
shashank <shas.srivastava@gmail.com>
2021-09-04 00:48:44 +0800

    Updated Data Preprocessing to output DataFrames instead of Lists

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -2,9 +2,15 @@
 """
 # Library imports
 import os
+from pandas import DataFrame
 
-def read_csv_data(src_path: 'str', filename: 'str') -> list:
+def read_raw_data(src_path: 'str', filename: 'str') -> DataFrame:
     file_path = os.path.join(src_path, filename)
     with open(file_path, 'r', encoding='utf-8-sig') as f:
-        lines = f.readlines()
-    return lines
+        data_list = f.readlines()
+    columns = data_list[0].split(',')
+    data = []
+    for i in range(1, len(data_list)):
+        data.append(data_list[i].split(','))
+    df = DataFrame(data = data, columns = columns)
+    return df
--- main.py
+++ main.py
@@ -3,14 +3,17 @@ Main file for US Traffic 2015 dataset challenge for OCBS Hack-IT 2021
 Data & AI Track
 """
 # Import libraries
-from data_preprocessing import read_csv_data
+from data_preprocessing import read_raw_data
 
 # Datafile source directory and filename
-src = r'/Users/shashank/Documents/ocbc'
+src = r'C:\Users\Shashank.Srivastava\Documents\ocbc'
+
 filename_traffic = r'dot_traffic_2015.txt'
 filename_stations = r'dot_traffic_stations_2015.txt'
-raw_traffic = read_csv_data(src, filename_traffic)
-raw_stations = read_csv_data(src, filename_stations)
+
+# raw_traffic = read_raw_data(src, filename_traffic)
+raw_stations = read_raw_data(src, filename_stations)
 
 # Garbage collection
 del src, filename_stations, filename_traffic
+
